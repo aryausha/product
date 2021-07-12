68315b2aff3e55178352bfca6465135e8a1c7701
Aryamol A U <mearyamol@gmail.com>
2021-07-12 14:03:27 +0530

    add background colour

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,32 +9,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
-<body>
+<body class="pic">
     <table class="table table-borderless">
         <tr>
-            <td>PDTNAME</td>
+            <td style="color: rgb(255, 8, 0);">PDTNAME</td>
             <td><input type="text" class="form-control"></td>
         </tr>
         
         <tr>
-            <td>DESCRIPTION</td>
+            <td style="color: rgb(255, 8, 0);">DESCRIPTION</td>
             <td><input type="text"class="form-control"></td>
         </tr>
         <tr>
-            <td>PRICE</td>
+            <td style="color: rgb(255, 8, 0);">PRICE</td>
             <td><input type="text"class="form-control"></td>
         </tr>
         <tr>
-            <td>MANUFACTURE NAME</td>
+            <td style="color: rgb(255, 8, 0);" >MANUFACTURE NAME</td>
             <td><input type="text"class="form-control"></td>
         </tr>
         <tr>
-            <td>MANUFACTURE YEAR</td>
+            <td style="color: rgb(255, 8, 0);">MANUFACTURE YEAR</td>
             <td><input type="text"class="form-control"></td>
         </tr>
         <tr>
-            <td>GENDER</td>
-            <td><input type="radio" class="form-check-input" name="gender">male
+            <td style="color: rgb(255, 8, 0);">GENDER</td>
+            <td style="color: rgb(255, 8, 0);"><input type="radio" class="form-check-input" name="gender">male
                 <input type="radio" class="form-check-input" name="gender">female
             </td>
         </tr>
