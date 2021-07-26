a59d77f0611a4b9252edccc6eca5d7bfd06a73d7
Aryamol A U <mearyamol@gmail.com>
2021-07-26 23:18:16 +0530

    reading by js

--- index.php
+++ index.php
@@ -13,37 +13,82 @@
     <table class="table table-borderless">
         <tr>
             <td style="color: rgb(255, 8, 0);">PDTNAME</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" id="name" class="form-control"></td>
         </tr>
         
         <tr>
             <td style="color: rgb(255, 8, 0);">DESCRIPTION</td>
-            <td><input type="text"class="form-control"></td>
+            <td><input type="text" id="des" class="form-control"></td>
         </tr>
         <tr>
             <td style="color: rgb(255, 8, 0);">PRICE</td>
-            <td><input type="text"class="form-control"></td>
+            <td><input type="text" id="price" class="form-control"></td>
         </tr>
         <tr>
             <td style="color: rgb(255, 8, 0);" >MANUFACTURE NAME</td>
-            <td><input type="text"class="form-control"></td>
+            <td><input type="text" id="mname" class="form-control"></td>
         </tr>
         <tr>
             <td style="color: rgb(255, 8, 0);">MANUFACTURE YEAR</td>
-            <td><input type="text"class="form-control"></td>
+            <td><input type="text" id="myear" class="form-control"></td>
         </tr>
         <tr>
             <td style="color: rgb(255, 8, 0);">GENDER</td>
-            <td style="color: rgb(255, 8, 0);"><input type="radio" class="form-check-input" name="gender">male
-                <input type="radio" class="form-check-input" name="gender">female
+            <td style="color: rgb(255, 8, 0);">
+                <input type="radio" id="gender" class="form-check-input" name="gender" value="male">male
+                <input type="radio" id="gender1" class="form-check-input" name="gender" value="female">female
             </td>
         </tr>
         <tr>
             <td></td>
-            <td><button class="btn btn-danger">SUBMIT</button></td>
+            <td><button onclick="product()" class="btn btn-danger">SUBMIT</button></td>
         </tr>
 
 
     </table>
+
+
+    <script>
+        function product()
+        {
+            var name=document.getElementById("name").value
+            var desc=document.getElementById("des").value
+            var price=document.getElementById("price").value
+            var mname=document.getElementById("mname").value
+            var myear=document.getElementById("myear").value
+            var x=document.getElementById("gender").checked
+            var y=document.getElementById("gender1").checked
+            
+
+
+            if(x==true)
+            {
+                var getgender=document.getElementById("gender").value
+            }
+            else if(y==true)
+            {
+                var getgender=document.getElementById("gender1").value
+            }
+
+            console.log(name)
+            console.log(desc)
+            console.log(price)
+            console.log(mname)
+            console.log(myear)
+            console.log(getgender)
+            
+
+
+
+
+
+
+
+
+        }
+
+
+
+    </script>
 </body>
 </html>
